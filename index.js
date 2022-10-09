7009241ca9b3b67bcf6ec611b6fd1c4ce4b0648d
Said-Guzman <112099452+Said-Guzman@users.noreply.github.com>
2022-10-09 13:17:27 -0400

    updated

---
+++ index.js
@@ -0,0 +1,29 @@
+const dropdown = document.querySelector(".dropdown") // looks for the dropdown class
+const navMenu = document.querySelector(".nav-menu") // looks for the nav-menu
+
+dropdown.addEventListener("click",() =>{
+    dropdown.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}) // onclick it'll toggle the active portion of the menu, activating the dropdown and navmmenu
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: false, //slider will loop
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination', // page indicator
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  }); // no explanation for this  it's self explanitory..... Buttons 
+
+
+
+
+
+
