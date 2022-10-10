b17bc7bfabbbdea53536eb11f1d34dda9b28bd77
Said-Guzman <112099452+Said-Guzman@users.noreply.github.com>
2022-10-10 10:06:10 -0400

    updated stuff

--- javascript/index.js
+++ javascript/index.js
@@ -6,7 +6,7 @@ dropdown.addEventListener("click",() =>{
     navMenu.classList.toggle("active");
 }) // onclick it'll toggle the active portion of the menu, activating the dropdown and navmmenu
 
-const swiper = new swiper('.swiper', {
+const swiper = new Swiper('.swiper', {
     // Optional parameters
     loop: false, //slider will loop
   
